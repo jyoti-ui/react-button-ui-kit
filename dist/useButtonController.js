19e03737cde4a89bac8f7ed15647e4eb8328545f
Jyoti Singh <jyot5288@gmail.com>
2023-11-09 06:32:55 -0800

    compiled latest file structure with babel

--- dist/useButtonController.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-const useButtonController = () => {
-  const [text, setText] = useState("Hi");
-  return {
-    text,
-    setText
-  };
-};
-export default useButtonController;
